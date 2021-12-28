103bbbfb3398d3af4eae2ba0e8551b50f1df627a
JEANNOEL <nfonjeannoel1@gmail.com>
2021-12-28 12:01:51 -1100

    backup

--- dicescraper/dicescraper/spiders/dicebot.py
+++ dicescraper/dicescraper/spiders/dicebot.py
@@ -1,27 +1,31 @@
 import scrapy
+from ..helpers.dbconnect import Dbconnect
 
 
 def get_urls_from_db():
-    return ["https://www.dice.com/company/10110693a"]
+    with Dbconnect() as client:
+        data = client.get_CompanyUrl()
+    return data
 
 
 class DicebotSpider(scrapy.Spider):
     name = 'dicebot'
 
     def start_requests(self):
-        urls = get_urls_from_db()
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        data = get_urls_from_db()
+        for dt in data:
+            url = dt.get("companyPageUrl")
+            company_id = dt.get("id")
+            yield scrapy.Request(url=url, callback=self.parse, meta={"id": company_id})
 
     def parse(self, response):
+        company_id = response.meta.get('id')
         key_urls = response.css(".company-right:nth-child(1) a")
-        sites = []
+        urls_data = {}
         for site in key_urls:
-            sites.append({
-                "name": site.css("::text").get(default="NA").strip(),
-                "url": site.css("::attr(href)").get(default="NA")
+            urls_data.update({
+                "company_id": company_id,
+                site.css("::text").get(default="NA").strip(): site.css("::attr(href)").get(default="NA")
             })
-        yield {
-            "location": response.css(".location ::text").get(),
-            "key_urls": sites
-        }
+        with Dbconnect() as client:
+            client.SaveToDB(urls_data)
